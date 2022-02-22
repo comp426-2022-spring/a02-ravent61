50702230d6a27f592bc4c3d4de771081663c2399
Raven Taylor <raven61@live..unc.edu>
2022-02-22 16:29:28 -0500

    fixed notaword

--- guess-flip.js
+++ guess-flip.js
@@ -4,10 +4,11 @@ import { flipACoin } from "./modules/coin.mjs";
 const args = (process.argv.slice(2))
 const call = minimist(args)['call']
 
-
-if (call == null || call != 'heads' || call != "tails") {
+console.log(String(call))
+if (call == 'heads' || call == 'tails') {
+    console.log(flipACoin(call))
+}else{
     console.log('Error: no input.')
     console.log('Usage: node guess-flip --call=[heads|tails]')
     process.exit(1)
 }
-console.log(flipACoin(call))
